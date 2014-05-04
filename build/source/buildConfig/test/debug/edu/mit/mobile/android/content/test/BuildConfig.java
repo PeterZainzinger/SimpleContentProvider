93a77eb4a7f2cc164374ef144e3de157c482ef3f
Peter Zainzinger <peter.zainzinger@allaboutapps.at>
2014-05-04 10:28:31 +0200

    some changes

---
+++ build/source/buildConfig/test/debug/edu/mit/mobile/android/content/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.mit.mobile.android.content.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "edu.mit.mobile.android.content.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+}
